d3b16244dacc3f90f47ee27fe0f53caad21f3ba9
CRAZYROCK04 <crazytoysarg@gmail.com>
2024-12-13 20:15:08 -0300

    subiendo archivo

---
+++ src/app/components/ImageGallery.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+
+export default function ImageGallery () {
+    const [images, setImages] = useState ([]);
+    const [imageInput, setImageInput] = useState ("");
+}
